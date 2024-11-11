a72e251cbf2cf0f38aaa0677795713ce0fe26597
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-11 19:48:14 +0100

    add Django-Models

---
+++ django-models/relationship_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),  # Function-based view
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+    #Auth
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    #Roles
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+]

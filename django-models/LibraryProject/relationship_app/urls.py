f7b01731865fe3b0115c0288a394bf46a740c749
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-12 13:13:39 +0100

    Update views.py task2-second goal views.register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import list_books
+from .views import list_books , Register
 from django.contrib.auth.views import LoginView,LogoutView
+
 urlpatterns = [
     path('ListBook/',views.list_books, name="list_book"),
     path('library_detail/', views.LibraryDetailView.as_view(),name="library_detail"),
     path('login/',LoginView.as_view(template_name='relationship_app/login.html', name='login')), #User Login
     path('logout/',LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),#User Logout
+    path('register/', Register.as_view(), name='register'),#Register
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -19,8 +19,9 @@ class LibraryDetailView(DetailView):
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
 
-class register(CreateView):
+class Register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name='relationship_app/register.html'

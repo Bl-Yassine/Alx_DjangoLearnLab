63efa579d7eb7f7e903352cc5d35e7a93fb6f0fb
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-12 22:04:26 +0100

    update task4

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
     #permission_required
-    path('add_book',views.add_book,name="add_book"),
-    path('edit_book',views.change_book,name="edit_book"),
-    path('delete_book',views.change_book,name="delete_book"),
+    path('add_book/',views.add_book,name="add_book"),
+    path('edit_book/',views.change_book,name="edit_book"),
+    path('delete_book/',views.change_book,name="delete_book"),
 ]

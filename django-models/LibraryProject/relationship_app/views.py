30862bde92fbb0f2a05b578e7719ea63c7214a41
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-12 13:11:42 +0100

    Update views.py task2-second goal views.register

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -19,7 +19,6 @@ class LibraryDetailView(DetailView):
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth import login
 
 class register(CreateView):
     form_class = UserCreationForm

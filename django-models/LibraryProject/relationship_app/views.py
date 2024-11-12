269f3e1dbbe7c27b7329d51d9a02bacc59e01a45
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-12 12:59:02 +0100

    Update views.py task2-first goal

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,6 +15,18 @@ class LibraryDetailView(DetailView):
     model =Library
     template_name ='relationship_app/library_detail.html'
 
+#User Registration
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth import login
+
+class SignUpViwe(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name='relationship_app/register.html'
+
+
 
 
 

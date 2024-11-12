cd92f937b253e4952dbed6a4f1c25108783241f5
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-12 12:55:12 +0100

    Add task2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 from .views import list_books
-
+from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     path('ListBook/',views.list_books, name="list_book"),
-    path('', views.LibraryDetailView.as_view(),name="library_detail"),
+    path('library_detail/', views.LibraryDetailView.as_view(),name="library_detail"),
+    path('login/',LoginView.as_view(template_name='relationship_app/login.html', name='login')), #User Login
+    path('logout/',LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),#User Logout
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+
 # Create your views here.
 
+#Create a function-based view in relationship_app/views.py that lists all books stored in the database.
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html')
 
-
+#Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
 class LibraryDetailView(DetailView):
     model =Library
     template_name ='relationship_app/library_detail.html'
+
+
+
+
+

b32c593361062148da3ac862b60cc7a991ed4e96
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-12 13:22:14 +0100

    Update views.py task2-second goal views.register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import list_books , Register
+from .views import list_books , register
 from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
@@ -8,5 +8,5 @@ urlpatterns = [
     path('library_detail/', views.LibraryDetailView.as_view(),name="library_detail"),
     path('login/',LoginView.as_view(template_name='relationship_app/login.html', name='login')), #User Login
     path('logout/',LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),#User Logout
-    path('register/', Register.as_view(), name='register'),#Register
+    path('register/',views.register, name='register'),#Register
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -21,10 +21,9 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
 
-class Register(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name='relationship_app/register.html'
+def register(request):
+    form = UserCreationForm()
+    return render (request,"relationship_app/register.html")
 
 
 

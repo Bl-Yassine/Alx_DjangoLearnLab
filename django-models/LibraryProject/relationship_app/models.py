31977864b7dbfd3173e16e8ed7959fa7a5849944
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-12 16:56:00 +0100

    update task3

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -32,14 +32,28 @@ class Librarian(models.Model):
 
 #Creat UserProfile
 from django.contrib.auth.models import User
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_choices = [('Admin', 'Admin'),
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
         ('Librarian', 'Librarian'),
-        ('Member', 'Member'),]
-    role = models.CharField(max_length=10,choices= user_choices)
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+
+# Automatically create a UserProfile when a new User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
 

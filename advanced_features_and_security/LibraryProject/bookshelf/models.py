5cdf35d9ac41f56d488742301cb686930e876840
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-16 17:30:41 +0100

    update models

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,3 +5,12 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.DateField()
+
+
+#Set Up the costom User model
+from django.contrib.auth.models import AbstractUser
+
+class CostumUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
+    

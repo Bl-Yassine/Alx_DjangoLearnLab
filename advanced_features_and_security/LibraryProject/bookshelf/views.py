f753ec6f45d93716a5353cb8bf58b71a02f9cb60
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-22 21:23:58 +0100

    update security setting

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import Group , Permission
 from django.contrib.contenttypes.models import ContentType
 from .models import CustomUser
-
+from .forms import ExampleForm
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 @permission_required('bookshelf.can_view', raise_exception=True)  # Ensure the user has 'can_view' permission

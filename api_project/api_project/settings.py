fd888ac00076d9d1f04a6d3dd11e372f48ac0154
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-24 23:48:58 +0100

    update setting task3

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -131,4 +131,5 @@ REST_FRAMEWORK = [
     'rest_framework.authentication.SessionAuthentication',
     'rest_framework.authentication.BasicAuthentication',
     'rest_framework.authentication.TokenAuthentication',
+    'rest_framework.permissions.IsAuthenticated',
 ]

1f9e7d3c2be7a7dbb051fa2d5d3151e6b17b4ed7
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-30 11:25:01 +0100

    add serailizer

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import date 
+
 # Create your models here.
 
 # Author model
@@ -12,23 +12,6 @@ class Book(models.Model):
     publication_year = models.DateField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-# Create Custom Serializers
-from rest_framework import serializers
 
-# Create BookSerializer
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__' #all fields
 
-    def validate(self,publication_year):
-        if (publication_year > date.today):
-            raise serializers.ValidationError("the publication year should be in the present or in the past, not in the future")
-        return publication_year
-
-#Create AuthorSerializer
-class AuthorSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = ['name'] #name field
 
---
+++ advanced_api_project/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Book, Author
+from datetime import date 
+
+# Create BookSerializer
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__' #all fields
+
+    def validate(self,publication_year):
+        if (publication_year > date.today):
+            raise serializers.ValidationError("the publication year should be in the present or in the past, not in the future")
+        return publication_year
+
+#Create AuthorSerializer
+class AuthorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['name'] #name field

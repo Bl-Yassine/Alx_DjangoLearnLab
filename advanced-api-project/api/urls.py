bf84ed4907d7b8c39f742eac841f2f3478ce26a8
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-30 16:11:32 +0100

    Update api.urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path("api/", views.apiOverview, name="api_OverView"),
-    path("book_list/", views.ListView, name="ListView"),
+    path("books/", views.ListView, name="ListView"),
     path("book_detail/<str:pk>/", views.DetailView, name="DetailView"),
-    path("book_create/", views.CreateView, name="CreateView"),
-    path("book_update/<str:pk>/", views.UpdateView, name="UpdateView"),
-    path("book_delete/<str:pk>/", views.DeleteView, name="DeleteView"),
+    path("books/create/", views.CreateView, name="CreateView"),
+    path("books/update/<str:pk>/", views.UpdateView, name="UpdateView"),
+    path("bbooks/delete/<str:pk>/", views.DeleteView, name="DeleteView"),
 
 ]

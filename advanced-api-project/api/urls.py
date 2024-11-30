d86b9473463130a1036b06dd6916575b0c3a03e8
Yassine YBL <yassine.blila.95@gmail.com>
2024-11-30 16:08:26 +0100

    add task1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path("api/books", views.BookListCreateAPIView.as_view(), name="book_list"),
-    path("api/authors", views.AuthorListCreateAPIView.as_view(), name="book_list"),
+    path("api/", views.apiOverview, name="api_OverView"),
+    path("book_list/", views.ListView, name="ListView"),
+    path("book_detail/<str:pk>/", views.DetailView, name="DetailView"),
+    path("book_create/", views.CreateView, name="CreateView"),
+    path("book_update/<str:pk>/", views.UpdateView, name="UpdateView"),
+    path("book_delete/<str:pk>/", views.DeleteView, name="DeleteView"),
+
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,15 +7,56 @@ from rest_framework import generics
 from .models import Book ,Author
 from .serializers import BookSerializer ,AuthorSerializer
 
-#Book Views
 
-class BookListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+#Set Up Generic Views
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-class AuthorListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+@api_view(['GET'])
+def apiOverview(request):
+    api_url = {
+        'ListView':'/book_list/',
+        'DetailView':'/book_detail/<str:pk>/',
+        'CreateView':'/book_create/',
+        'UpdateView':'/book_update/<str:pk>/',
+        'DeleteView':'/book_delete/<str:pk>/',
+    }
+    return Response(api_url)
 
+#ListView
+@api_view(['GET'])
+def ListView(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books , many=True)
+    return Response(serializer.data)
 
+#DetailView 
+@api_view(['GET'])
+def DetailView(request, pk):
+    books = Book.objects.get(id=pk)
+    serializer = BookSerializer(books , many=False)
+    return Response(serializer.data)
 
+#CreateView 
+@api_view(['POST'])
+def CreateView(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+#UpdateView 
+@api_view(['POST'])
+def UpdateView(request, pk):
+    books = Book.objects.get(id=pk)
+    serializer = BookSerializer(isinstance= books ,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+#DeleteView  
+@api_view(['DELETE'])
+def DeleteView(request, pk):
+    books = Book.objects.get(id=pk)
+    books.delete()
+    return Response("Succesfully delete!")

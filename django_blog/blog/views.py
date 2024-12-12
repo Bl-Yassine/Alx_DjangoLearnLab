beb95b802a24f1872532593c22da63e521c7a222
Yassine YBL <yassine.blila.95@gmail.com>
2024-12-12 13:52:04 +0100

    updtar static

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import RegisterUserForm
+
 class RegisterUser(CreateView):
     form_class = RegisterUserForm
     success_url = reverse_lazy('login')

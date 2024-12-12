7f28d7d8d9768d721e1a50822ea285d0e953ffb0
Yassine YBL <yassine.blila.95@gmail.com>
2024-12-12 16:41:17 +0100

    add login html

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('register/',views.RegisterUser.as_view() , name ='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]

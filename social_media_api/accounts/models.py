7de9f989ba7c30913c5b6b305d79c39ec39b30ab
Yassine YBL <yassine.blila.95@gmail.com>
2024-12-20 22:56:32 +0100

    Update bio field type to text

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,7 +6,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    bio = models.CharField(max_length=200)
+    bio = models.TextField()
     profile_picture=models.ImageField()
     followers=models.ManyToManyField('self',symmetrical=False)
 

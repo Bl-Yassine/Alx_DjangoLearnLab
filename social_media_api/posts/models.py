1c3044827ced64d78a847c47603c69c48ec6a6db
Yassine YBL <yassine.blila.95@gmail.com>
2024-12-21 20:35:07 +0100

    update models

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -8,8 +8,8 @@ class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField()
-    created_at = models.DateField(auto_now=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title

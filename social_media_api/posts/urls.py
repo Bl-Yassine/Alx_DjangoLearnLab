b940ef4d94bef3a49fdbd86d1389327b8d73cb35
Yassine YBL <yassine.blila.95@gmail.com>
2024-12-22 11:53:14 +0100

    update views task1

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,19 +1,11 @@
-from django.urls import path 
-from . import views
+from django.urls import path, include
+from .views import PostViewSet , CommentViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
-
-    #Post CRUD URLS
-    path('post/',views.PostListView.as_view(), name='list_post'),
-    path('post/create/',views.PostCreateView.as_view(), name='create_post'),
-    path('post/<int:pk>/',views.PostRetrieveView.as_view(), name='retrieve_post'),
-    path('post/<int:pk>/update/',views.PostUpdateView.as_view(), name='update_post'),
-    path('post/<int:pk>/delete/',views.PostDeleteView.as_view(), name='delete_post'),
-
-    #Comment CRUD URLS
-    path('Comment/',views.CommentListView.as_view(), name='comment_post'),
-    path('Comment/create/',views.CommentCreateView.as_view(), name='comment_post'),
-    path('Comment/<int:pk>/',views.CommentRetrieveView.as_view(), name='comment_post'),
-    path('Comment/<int:pk>/update/',views.CommentUpdateView.as_view(), name='comment_post'),
-    path('Comment/<int:pk>/delete/',views.CommentDeleteView.as_view(), name='comment_post'),
+    path('',include(router.urls)),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,55 +1,15 @@
 from django.shortcuts import render
 from .models import Post , Comment
 from .serializers import PostSerializers, CommentSerializers
-from rest_framework.generics import ListAPIView , CreateAPIView ,RetrieveAPIView, UpdateAPIView ,DestroyAPIView
+from rest_framework import viewsets
 # Create your views here.
 
-#Post-CreatView
-class PostCreateView(CreateAPIView):
-    serializer_class = PostSerializers
-
-#Post-listView
-class PostListView(ListAPIView):
-    serializer_class = PostSerializers
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-
-#Post-RetrieveView
-class PostRetrieveView(RetrieveAPIView):
     serializer_class = PostSerializers
-    queryset = Post.objects.all()
 
-#Post-UpdateView
-class PostUpdateView(UpdateAPIView):
-    serializer_class = PostSerializers
-    queryset = Post.objects.all()
-
-#Post-DeleteView
-class PostDeleteView(DestroyAPIView):
-    serializer_class = PostSerializers
-    queryset = Post.objects.all()
-
-
-
-#Comment-CreatView
-class CommentCreateView(CreateAPIView):
-    serializer_class = CommentSerializers
-
-#Comment-listView
-class CommentListView(ListAPIView):
-    serializer_class = CommentSerializers
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
-
-#Comment-RetrieveView
-class CommentRetrieveView(RetrieveAPIView):
     serializer_class = CommentSerializers
-    queryset = Comment.objects.all()
 
-#Comment-UpdateView
-class CommentUpdateView(UpdateAPIView):
-    serializer_class = CommentSerializers
-    queryset = Comment.objects.all()
 
-#Comment-DeleteView
-class CommentDeleteView(DestroyAPIView):
-    serializer_class = CommentSerializers
-    queryset = Comment.objects.all()
